70243e1d099831a74b78aa7eda2c6fbcbe97a2a4
dgyim <yimdgkr@gmail.com>
2023-03-18 19:17:12 +0900

    계정_등록_여부에_따른_로그인_테스트 추가

--- src/test/java/com/chatserver/dgyim/LoginProcessTest.java
+++ src/test/java/com/chatserver/dgyim/LoginProcessTest.java
@@ -2,6 +2,8 @@ package com.chatserver.dgyim;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,35 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 //trycount를 초과했을 때
 //올바른 input값이 주어졌을 때 로그인이 성공하는가?
 class LoginProcessTest {
-    @Test
-    void 등록된_계정_로그인_통과_테스트() {
+    @ParameterizedTest
+    @CsvSource({
+            "dong, 123, dong 123, true",
+            "dong, 123, gyu 123, false"
+    })
+    void 계정_등록_여부에_따른_로그인_테스트(String registeredUsername, String registeredPassword, String loginLine, boolean expect) {
         //given
         LoginManager loginManager = LoginManager.createEmpty();
-        loginManager.register("dong", "123");
+        loginManager.register(registeredUsername, registeredPassword);
         LoginProcess loginProcess = new LoginProcess(2, loginManager);
-        String loginLine = "dong 123";
 
         //when
         boolean actual = loginProcess.tryLogin(loginLine);
 
         //then
-        Assertions.assertThat(actual).isTrue();
-    }
-
-//    이거 나중에 parameterTest로 바꾸기
-    @Test
-    void 등록되지_않은_로그인_실패_테스트() {
-        //given
-        LoginManager loginManager = LoginManager.createEmpty();
-        loginManager.register("dong", "123");
-        LoginProcess loginProcess = new LoginProcess(2, loginManager);
-        String loginLine = "gyu 123";
-
-        //when
-        boolean actual = loginProcess.tryLogin(loginLine);
-
-        //then
-        Assertions.assertThat(actual).isFalse();
+        Assertions.assertThat(actual).isEqualTo(expect);
     }
 
     @Test
